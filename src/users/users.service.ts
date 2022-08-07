09737ba8bce4e63e73700e0560f90843e39a22e3
Ibraheem Muhammadawwal <Ibraheemmuhdawwal@gmail.com>
2022-08-07 10:20:01 +0100

    user-create

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+  ) {}
+
+  async create(email: string, password: string, name: string): Promise<User> {
+    const user = this.usersRepository.create({ email, password, name });
+    return this.usersRepository.save(user);
+  }
+}
